ea032cc7d29787cfe0e6c1feea4d618307d5fd40
Anthony Córdova <anthonycordova@gmail.com>
2021-06-25 09:47:46 -0500

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
